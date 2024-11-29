52d7f454c68997e4650ef663fa95c2784dc390d6
OmmDevgoswami <teamuniverse.omm@gmail.com>
2024-11-30 01:03:23 +0530

    Header and Footer Added !!

--- Ready-to-Play/src/App.jsx
+++ Ready-to-Play/src/App.jsx
@@ -1,20 +1,33 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Loading from './components/Loading.jsx';
-import HomePage from './components/Homepage.jsx';
-import './App.css'
+import Home from './components/Home.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import './App.css';
 
 function App() {
+  const location = useLocation();
+  const isLoading = location.pathname === '/';
+
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Loading />} />
-          <Route path="/HomePage" element={<HomePage />} />
-        </Routes>
-      </Router>
+      {!isLoading && <Header />}
+      <Routes>
+        <Route path="/" element={<Loading />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+      {!isLoading && <Footer />}
     </>
   );
 }
 
-export default App
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default AppWrapper;
--- Ready-to-Play/src/components/Loading.jsx
+++ Ready-to-Play/src/components/Loading.jsx
@@ -1,16 +1,20 @@
+import { Link } from 'react-router-dom';
 import './Loading.css';
-import HomePage from './Homepage.jsx'
+import logo from '../assets/Resources/Logo.png'
+
 
 function Loading() {
   return (
     <>
         <div className="banner">
             <div className="container">
-                <h1>
-                    <img className="logo" src="./src/assets/Resources/Logo.png" alt="logo" /> 
+                <h1>       
+                    <img className="logo" src={logo} alt="logo" /> 
                     Ready-to-Play
                 </h1>
-                <button type="button" className="btn btn-secondary btn-lg"><a href="HomePage"> Let The Rumbling Begin </a></button>
+                <Link to="/home">
+                    <button type="button" className="btn btn-secondary btn-lg"> Let The Rumbling Begin </button>
+                </Link>
             </div>
       </div>
     </>
